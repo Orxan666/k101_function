f9281d62b301d94c99c36c34cda21003c2ed69ea
orxan666 <orxanallahyarov03@gmail.com>
2022-11-27 16:41:11 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// var a=5
+// var b=3
+// var total=a+b
+
+// console.log(total);
+
+// function topla(a,b){
+//     console.log(a+b);
+// }
+
+// topla(4,5)
+// topla(2,4)
+// console.log(a);
+// var a=6
+
+// function evli(c,d){
+//     return c+" ve "+d+" xosbext olsunlar";
+// }
+
+// console.log(evli("Eli","Aysel"));
+// -----------------
+// decleration function
+// function cixma(a,b,c){
+// return a-b-c
+// }
+
+// console.log(cixma(9,5,1));
+
+// ----------------------
+// experssion function
+
+// var cixma = function (reqem1, reqem2) {
+//   return reqem1 - reqem2;
+// };
+// console.log(cixma(4,1));
+
+// ----------------
+// UNNAMED FUNCTION
+// var h1=document.querySelector(".name1")
+// h1.addEventListener("click",function(){
+// this.style.color="red"
+// })
+
